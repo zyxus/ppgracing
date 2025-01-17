e4808bca9077e109fff5d4cbfd48247448a0be1f
Alex Popov <7996096@gmail.com>
2025-01-18 00:05:28 +0200

    добавил формочку сделал компонент - кнопка регистрации попытался стилизовать но не все вышло

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,15 @@
-import '../css/app.scss';
+import '../sass/app.scss';
 
 import { createApp } from 'vue';
+
 import Greeting from './components/Welcome.vue';
+
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
+import '@material/web/textfield/filled-text-field.js';
+import '@material/web/textfield/outlined-text-field.js';
+import '@material/web/fab/branded-fab.js';
+import '@material/web/icon/icon.js';
 
 const app = createApp(Greeting);
 app.mount('#app');
--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,21 +1,43 @@
-<template>
-    <div class="greeting">
-        <h1>Привет, мир!</h1>
-        <md-filled-button label="Нажми меня!">Нажми меня!</md-filled-button>
-    </div>
-</template>
 
 <script>
+import RegisterButton from "./RegisterButton.vue";
+
 export default {
+    components: {RegisterButton},
     mounted() {
         // Для базовой кнопки можно не использовать дополнительную настройку
     }
 };
 </script>
 
+<template>
+    <div class="container">
+        <md-filled-text-field required label="PSN"></md-filled-text-field>
+        <md-filled-text-field required label="GT7 Nickname" ></md-filled-text-field>
+        <md-filled-text-field required label="Имя"></md-filled-text-field>
+        <md-filled-text-field required label="Фамилия"></md-filled-text-field>
+        <md-filled-text-field required label="Город, страна"></md-filled-text-field>
+        <md-filled-text-field required label="Оборудование (модель руля/геймпад)"></md-filled-text-field>
+        <md-filled-text-field required label="Дата и время отборочного заезда"></md-filled-text-field>
+        <md-filled-text-field required label="Команда"></md-filled-text-field>
+
+        <RegisterButton />
+
+    </div>
+</template>
+
+
 <style scoped>
-.greeting {
-    text-align: center;
-    margin-top: 50px;
+
+
+    /* Стили для текстовых полей */
+md-filled-text-field {
+    --md-sys-color-surface: #fff; /* Белый фон */
+    --md-sys-color-on-surface: #000; /* Черный текст */
+    --md-sys-color-outline: #ccc; /* Серый контур */
+    --md-filled-text-field-focus-active-indicator-color: #e17740;
+    --md-filled-text-field-focus-active-label-text-color: #e17740;
+
+    font-family: 'Roboto', sans-serif;
 }
 </style>
