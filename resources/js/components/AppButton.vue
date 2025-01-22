e8e619e7e0c91030613639f28a437b4c0f438b76
del_maradinho <dmitriev.a@technoavia.ru>
2025-01-22 14:25:37 +0500

    кастомные стили и цвета

--- resources/js/components/AppButton.vue
+++ resources/js/components/AppButton.vue
@@ -1,10 +1,3 @@
 <template>
-    <button class="btn btn-primary custom-button">Регистрация</button>
+    <button type="submit" class="btn btn-primary custom-button">Регистрация</button>
 </template>
-
-<style scoped>
-.custom-button {
-    margin: 32px 0 0;
-    border-radius: 5px; /* Скругление углов */
-}
-</style>
--- resources/js/components/UserRegistrationForm.vue
+++ resources/js/components/UserRegistrationForm.vue
@@ -1,47 +1,48 @@
 <template>
     <form @submit.prevent="onSubmit" class="form">
         <div class="mb-3">
-            <label for="psn" class="form-label">PSN</label>
-            <input type="text" id="psn" class="form-control" required />
+            <label for="psn" class="form-label label-custom">PSN</label>
+            <input type="text" id="psn" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="nickname" class="form-label">GT7 Nickname</label>
-            <input type="text" id="nickname" class="form-control" required />
+            <label for="nickname" class="form-label label-custom">GT7 Nickname</label>
+            <input type="text" id="nickname" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="firstName" class="form-label">Имя</label>
-            <input type="text" id="firstName" class="form-control" required />
+            <label for="firstName" class="form-label label-custom">Имя</label>
+            <input type="text" id="firstName" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="lastName" class="form-label">Фамилия</label>
-            <input type="text" id="lastName" class="form-control" required />
+            <label for="lastName" class="form-label label-custom">Фамилия</label>
+            <input type="text" id="lastName" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="location" class="form-label">Город, страна</label>
-            <input type="text" id="location" class="form-control" required />
+            <label for="location" class="form-label label-custom">Город, страна</label>
+            <input type="text" id="location" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="equipment" class="form-label">Оборудование (модель руля/геймпад)</label>
-            <input type="text" id="equipment" class="form-control" required />
+            <label for="equipment" class="form-label label-custom">Оборудование (модель руля/геймпад)</label>
+            <input type="text" id="equipment" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="qualifyingDate" class="form-label">Дата и время отборочного заезда</label>
-            <input type="datetime-local" id="qualifyingDate" class="form-control" required />
+            <label for="qualifyingDate" class="form-label label-custom">Дата и время отборочного заезда</label>
+            <input type="datetime-local" id="qualifyingDate" class="form-control custom-input" required />
         </div>
 
         <div class="mb-3">
-            <label for="team" class="form-label">Команда</label>
-            <input type="text" id="team" class="form-control" />
+            <label for="team" class="form-label label-custom">Команда</label>
+            <input type="text" id="team" class="form-control custom-input" />
         </div>
 
-        <button type="submit" class="btn btn-primary w-100">Submit</button>
+        <button type="submit" class="btn btn-primary btn-lg custom-button">Регистрация</button>
     </form>
+    <AppButton />
 </template>
 
 <script setup>
@@ -50,10 +51,35 @@ function onSubmit() {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@use '../../sass/colors.scss' as *;
+
+.custom-button {
+    width: 100%;
+    margin: 32px 0 0;
+    border-radius: 5px;
+    background-color: $primaryCustom;
+    border: none;
+    color: $textCustom;
+
+    &:hover {
+        background-color: $primaryCustomHover;
+        color: $textCustom;
+    }
+}
+
 .form {
     max-width: 400px;
     margin: 0 auto;
     padding: 16px;
+}
+
+.label-custom {
+    color: $textCustom;
+}
+
+.custom-input:focus {
+    border-color: $primaryCustom;
+    box-shadow: 0 0 0 0.1rem $primaryCustomHover;
 }
 </style>
